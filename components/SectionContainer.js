01a8d769fe8cc1a663972953bc8bcd4f5e32ed3b
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-09-29 14:08:31 -0700

    Use container

---
+++ components/SectionContainer.js
@@ -0,0 +1,25 @@
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+export default function SectionContainer(props) {
+  return (
+    <>
+      <Container component='main' maxWidth='sm' sx={{ mb: 4 }}>
+        <Paper
+          sx={{
+            my: { xs: 3, md: 6 },
+            p: { xs: 2, md: 3 },
+            backgroundColor: 'WhiteSmoke',
+          }}
+        >
+          <Typography component='h1' variant='h6' align='left-center'>
+            {props.icon}
+            {props.title}
+          </Typography>
+          {props.children}
+        </Paper>
+      </Container>
+    </>
+  );
+}
