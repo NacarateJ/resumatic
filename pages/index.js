384a4fea46973a0a70815b518a193fd0f7bf5289
IrinaGM <52331753+IrinaGM@users.noreply.github.com>
2023-10-01 11:48:05 -0400

    Revert "Feature/navbar and footer"

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import { Inter } from 'next/font/google';
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
       <h1>Welcome to Resumatic, your go-to resume builder app 🚀</h1>
     </main>
   );
