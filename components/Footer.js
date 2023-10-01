41805141c0e5888f6ad00d4d9fe9ee198b13fb55
Irina G <irina.golionko@gmail.com>
2023-10-01 13:44:13 -0400

    fix for hydration error

--- components/Footer.js
+++ components/Footer.js
@@ -3,23 +3,23 @@ import CopyrightIcon from '@mui/icons-material/Copyright';
 
 export default function Footer() {
   return (
-    <Typography variant='body2' align='center' sx={{ fontFamily: 'inherit' }}>
-      <Box
-        sx={{
-          padding: 4,
-          m: 2,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '10px',
-        }}
-      >
-        <CopyrightIcon />{' '}
+    <Box
+      sx={{
+        padding: 4,
+        m: 2,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Typography variant='body2' align='center' sx={{ fontFamily: 'inherit' }}>
+        <CopyrightIcon sx={{ marginRight: '5px' }} />
         <Link sx={{ color: '#FF5400' }} href='/' underline='none'>
           RESUMATIC
-        </Link>{' '}
+        </Link>
+        <span style={{ margin: '0 5px' }} />
         {new Date().getFullYear()}
-      </Box>
-    </Typography>
+      </Typography>
+    </Box>
   );
 }
