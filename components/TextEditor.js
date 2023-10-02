1fceed94df492d7df7a8c7264ca1283d9605f839
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-01 23:43:11 -0700

    Add button to toggle bold functionality
    Also added other icons we may need.

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,18 +1,53 @@
-import React, { useState } from 'react';
-import { createEditor } from 'slate';
+import React, { useState, useCallback } from 'react';
+import { createEditor, Editor, Transforms } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
-import { Box } from '@mui/material';
+import { Box, Toolbar, IconButton } from '@mui/material';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatIndentDecreaseIcon from '@mui/icons-material/FormatIndentDecrease';
+import FormatIndentIncreaseIcon from '@mui/icons-material/FormatIndentIncrease';
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
+import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
+import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
+import CustomEditor from '@/utils/CustomEditor';
+import Leaf from '@/utils/Leaf';
+import DefaultElement from '@/utils/DefaultElement';
+import CodeElement from '@/utils/CodeElement';
 
 
 export default function TextEditor({ generatedSummary }) {
+  // Create a Slate editor object that won't change across renders.
+  const [editor] = useState(() => withReact(createEditor()));
+
   const initialState = [
     {
       type: 'paragraph',
       children: [{ text: generatedSummary }],
     },
   ];
-  // Create a Slate editor object that won't change across renders.
-  const [editor] = useState(() => withReact(createEditor()));
+
+  // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
+  // Define a leaf rendering function that is memoized with `useCallback`.
+  const renderLeaf = useCallback((props) => {
+    return <Leaf {...props} />;
+  }, []);
+
+  // Function to toggle bold mark
+  const toggleBold = () => {
+    CustomEditor.toggleBoldMark(editor);
+  };
 
   return (
     <Box
@@ -24,9 +59,53 @@ export default function TextEditor({ generatedSummary }) {
         borderRadius: '5px',
       }}
     >
+      <Toolbar>
+        <IconButton>
+          <FormatListBulletedIcon />
+        </IconButton>
+
+        <IconButton onClick={toggleBold}>
+          <FormatBoldIcon />
+        </IconButton>
+
+        <IconButton>
+          <FormatIndentDecreaseIcon />
+        </IconButton>
+
+        <IconButton>
+          <FormatIndentIncreaseIcon />
+        </IconButton>
+
+        <IconButton>
+          <FormatItalicIcon />
+        </IconButton>
+
+        <IconButton>
+          <FormatAlignJustifyIcon />
+        </IconButton>
+
+        <IconButton>
+          <FormatAlignCenterIcon />
+        </IconButton>
+
+        <IconButton>
+          <FormatAlignLeftIcon />
+        </IconButton>
+
+        <IconButton>
+          <FormatAlignRightIcon />
+        </IconButton>
+      </Toolbar>
       <Slate editor={editor} initialValue={initialState}>
-        <Editable style={{ outline: 'none' }} />
+        <Editable
+          style={{ outline: 'none' }}
+          renderElement={renderElement}
+          // Pass in the `renderLeaf` function.
+          renderLeaf={renderLeaf}
+        />
       </Slate>
     </Box>
   );
 }
+
+
