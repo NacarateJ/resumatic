365bfed1a67899f5eb0c15430e274597bc130e5f
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-04 10:55:18 -0700

    Update resume with enhanced Summary

--- pages/api/profileSectionInsert.js
+++ pages/api/profileSectionInsert.js
@@ -3,14 +3,32 @@ import prisma from '@/prisma/prisma';
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      console.log('Request Body:', req.body);
-      // const { enhancedSummary } = req.body;
+      const data = req.body;
+      const { resumeId, enhancedSummary } = data;
 
-      //  const resume = await prisma.resumes.findUnique({
-      //    where: {
-           
-      //    },
-      //  });
+      // Check if the provided resumeId exists in the Resumes table
+      const existingResume = await prisma.resumes.findUnique({
+        where: {
+          resume_id: resumeId,
+        },
+      });
+
+      if (!existingResume) {
+        return res.status(404).json({ error: 'Resume not found' });
+      }
+
+      // Update the existing resume with the provided data
+          const updatedResume = await prisma.resumes.update({
+            where: {
+              resume_id: resumeId,
+            },
+            data: {
+              profile_description: enhancedSummary,
+            }
+          });
+
+          res.status(200).json({ resumeId: updatedResume.resume_id });
+          
     } catch (err) {
       console.log('Error fetching resumes:', err);
       // Internal server error
