d11eb59e1e7aa03ff97b47bd65471da81d0607f3
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-04 09:34:36 -0700

    Rename API to match others

--- pages/api/profileSectionInsert.js
+++ pages/api/profileSectionInsert.js
@@ -1,16 +1,16 @@
-import prisma from '@/utils/prisma';
+import prisma from '@/prisma/prisma';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       console.log('Request Body:', req.body);
-      const { enhancedSummary } = req.body;
+      // const { enhancedSummary } = req.body;
 
-       const resume = await prisma.resumes.findUnique({
-         where: {
+      //  const resume = await prisma.resumes.findUnique({
+      //    where: {
            
-         },
-       });
+      //    },
+      //  });
     } catch (err) {
       console.log('Error fetching resumes:', err);
       // Internal server error
