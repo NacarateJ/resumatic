bd1fae8abd7a35f0d0edabe853baa46838277da4
Irina G <irina.golionko@gmail.com>
2023-10-09 18:29:02 -0400

    change the login component on the login page

--- components/UserManagement/Login.js
+++ components/UserManagement/Login.js
@@ -1,12 +1,22 @@
 import Link from 'next/link';
 import SectionContainer from '../SectionContainer';
-import { Box, Grid, TextField, Button, Typography } from '@mui/material';
+import {
+  Box,
+  Grid,
+  TextField,
+  Button,
+  Typography,
+  Container,
+} from '@mui/material';
+import { useRouter } from 'next/router';
 
 export default function Login() {
+  const router = useRouter();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     // Access the form input fields using the event object
-    const formData = new FormData(event.target);
+    const formData = new FormData(event.currentTarget);
 
     // Extract specific form field values
     const email = formData.get('email');
@@ -15,74 +25,82 @@ export default function Login() {
     // Now you can use the `email` and `password` values as needed
     console.log('Email:', email);
     console.log('Password:', password);
+    router.push(`/dashboard`);
   };
 
   return (
-    <SectionContainer>
-      <Box
-        component='form'
-        onSubmit={handleSubmit}
-        sx={{
-          mt: 3,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component='h1' variant='h5'>
-          Login
-        </Typography>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <TextField
-              required
-              id='email'
-              name='email'
-              label='Email'
-              fullWidth
-              autoComplete='email'
-              variant='filled'
-              inputProps={{ style: { backgroundColor: 'white' } }}
-              sx={{ mt: 2 }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              required
-              id='password'
-              name='password'
-              label='Password'
-              type='password'
-              autoComplete='current-password'
-              variant='filled'
-              fullWidth
-              inputProps={{ style: { backgroundColor: 'white' } }}
-            />
-          </Grid>
-        </Grid>
-        <Grid container display='flex' direction='column' alignItems='center'>
-          <Button
-            type='submit'
-            variant='contained'
-            sx={{ mt: 3, ml: 1 }}
-            style={{
-              backgroundColor: '#00B4D8',
-            }}
-          >
+    <Container id='container-login' sx={{ mt: '5%' }}>
+      <SectionContainer>
+        <Box
+          id='login-box'
+          component='form'
+          onSubmit={handleSubmit}
+          sx={{
+            mt: 3,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component='h1' variant='h4' sx={{ mb: 3 }}>
             Login
-          </Button>
-          <Link key='create-account' href={`/new-account`} passHref>
+          </Typography>
+          <Grid
+            container
+            spacing={3}
+            id='grid-text-boxes'
+            style={{ justifyContent: 'center' }}
+          >
+            <Grid item xs={10}>
+              <TextField
+                required
+                id='email'
+                name='email'
+                label='Email'
+                fullWidth
+                autoComplete='email'
+                variant='filled'
+                inputProps={{ style: { backgroundColor: 'white' } }}
+              />
+            </Grid>
+            <Grid item xs={10}>
+              <TextField
+                required
+                id='password'
+                name='password'
+                label='Password'
+                type='password'
+                autoComplete='current-password'
+                variant='filled'
+                fullWidth
+                inputProps={{ style: { backgroundColor: 'white' } }}
+              />
+            </Grid>
+          </Grid>
+          <Grid container display='flex' direction='column' alignItems='center'>
             <Button
-              sx={{ mt: 1, ml: 1 }}
+              type='submit'
+              variant='contained'
+              sx={{ mt: 3, ml: 1 }}
               style={{
-                color: '#00B4D8',
+                backgroundColor: '#00B4D8',
               }}
             >
-              Create Account
+              Login
             </Button>
-          </Link>
-        </Grid>
-      </Box>
-    </SectionContainer>
+            <Link key='create-account' href={`/new-account`} passHref>
+              <Button
+                sx={{ mt: 1, ml: 1 }}
+                style={{
+                  color: '#00B4D8',
+                }}
+              >
+                Create Account
+              </Button>
+            </Link>
+          </Grid>
+        </Box>
+      </SectionContainer>
+    </Container>
   );
 }
